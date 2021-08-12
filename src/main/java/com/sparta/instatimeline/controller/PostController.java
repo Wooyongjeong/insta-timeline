7313a3b833b8d2004620a27ae74ae9e431ffa025
SSG\212575 <w_dragon@shinsegae.com>
2021-08-12 13:38:49 +0900

    소식(Posts) 관련 컨트롤러 PostController 작성

--- src/main/java/com/sparta/instatimeline/controller/PostController.java
+++ src/main/java/com/sparta/instatimeline/controller/PostController.java
@@ -1,22 +1,49 @@
 package com.sparta.instatimeline.controller;
 
+import com.sparta.instatimeline.domain.Post;
 import com.sparta.instatimeline.service.PostService;
+import com.sparta.instatimeline.utils.FileUploadUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
-@RequestMapping("/")
 @RequiredArgsConstructor
+@Slf4j
 public class PostController {
 
     private final PostService postService;
 
-    @GetMapping
-    public String home(Model model) {
+    @GetMapping("/posts/new")
+    public String createForm(Model model) {
+        log.info("Post create form");
+        model.addAttribute("form", new PostForm());
+        return "posts/createPostForm";
+    }
 
-        return "home";
+    @PostMapping("/posts/new")
+    public String create(PostForm form) throws IOException {
+        log.info("Post create");
+
+        MultipartFile image = form.getImage();
+        String fileName = StringUtils.cleanPath(image.getOriginalFilename());
+        FileUploadUtil.saveFile(FileUploadUtil.uploadDir, fileName, image);
+
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts")
+    public String list(Model model) {
+        List<Post> posts = postService.findPosts();
+        model.addAttribute("posts", posts);
+        return "posts/postList";
     }
 }
