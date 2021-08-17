fcbc3f8e32f3f03300ca7d1d9b2de2599e445fba
SSG\212575 <w_dragon@shinsegae.com>
2021-08-17 16:33:14 +0900

    Post 도메인 변경
    - Timestamped 삭제 및 Post에 createdAt, modifiedAt 추가
    
    - 생성 메서드 추가

--- src/main/java/com/sparta/instatimeline/domain/Post.java
+++ src/main/java/com/sparta/instatimeline/domain/Post.java
@@ -1,18 +1,21 @@
 package com.sparta.instatimeline.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "posts")
-public class Post extends Timestamped {
+public class Post {
 
     @Id @GeneratedValue
     private Long id;
@@ -23,11 +26,23 @@ public class Post extends Timestamped {
     @Column(nullable = false)
     private String contents;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public String getCreatedAt() {
+        return createdAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
     //생성 메서드
     public static Post createPost(String username, String contents) {
         Post post = new Post();
         post.setUsername(username);
         post.setContents(contents);
+        post.setCreatedAt(LocalDateTime.now());
+        post.setModifiedAt(LocalDateTime.now());
 
         return post;
     }
