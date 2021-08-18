283fc841d47505edebf71a0dbb03af39a6a9fb5d
wooyongjeong <wooyong.dev@gmail.com>
2021-08-18 14:15:38 +0900

    PostService의 savePost, deleteOne 변경
    - Post 생성 메서드에서 파일 확장자를 인자로 받도록 변경하였으므로 이를 적용
    
    - Post를 지우면 서버 내에 있는 사진 파일도 지우도록 추가

--- src/main/java/com/sparta/instatimeline/service/PostService.java
+++ src/main/java/com/sparta/instatimeline/service/PostService.java
@@ -3,13 +3,14 @@ package com.sparta.instatimeline.service;
 import com.sparta.instatimeline.controller.PostForm;
 import com.sparta.instatimeline.domain.Post;
 import com.sparta.instatimeline.repository.PostRepository;
-import com.sparta.instatimeline.utils.FileUploadUtil;
+import com.sparta.instatimeline.utils.PhotoUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -25,11 +26,11 @@ public class PostService {
     public Long savePost(PostForm form) throws IOException {
         log.info("PostService savePost");
 
-        FileUploadUtil.saveFile(form);
+        String extension = PhotoUtil.savePhoto(form);
 
         String username = form.getUsername();
         String contents = form.getContents();
-        Post post = Post.createPost(username, contents);
+        Post post = Post.createPost(username, contents, extension);
 
         postRepository.save(post);
         return post.getId();
@@ -62,9 +63,14 @@ public class PostService {
     }
 
     @Transactional
-    public Long deleteOne(Long postId) {
+    public Long deleteOne(Long postId) throws NoSuchFileException {
         log.info("PostService deleteOne " + postId);
 
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("해당하는 소식이 존재하지 않습니다.")
+        );
+        String fileName = post.getPhoto().photoFileName();
+        PhotoUtil.deletePhoto(fileName);
         postRepository.deleteById(postId);
         return postId;
     }
