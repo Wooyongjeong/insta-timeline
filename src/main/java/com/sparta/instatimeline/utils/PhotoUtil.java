a85c5ba938d584dceb4fba6fb1e1763974932124
wooyongjeong <wooyong.dev@gmail.com>
2021-08-18 14:12:31 +0900

    FileUploadUtil -> PhotoUtil로 파일명 rename 및 함수명 rename, deletePhoto 함수 추가
    - savePhoto 함수는 jpg만 처리하지 않고 모든 image에 대해 처리할 수 있도록 변경
    
    - deletePhoto는 인자로 넘어온 fileName을 삭제하도록 작성

--- src/main/java/com/sparta/instatimeline/utils/PhotoUtil.java
+++ src/main/java/com/sparta/instatimeline/utils/PhotoUtil.java
@@ -1,23 +1,28 @@
 package com.sparta.instatimeline.utils;
 
 import com.sparta.instatimeline.controller.PostForm;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class FileUploadUtil {
+@Slf4j
+public class PhotoUtil {
 
     private static Path uploadDir = Paths.get("images/");
 
-    public static void saveFile(PostForm form) throws IOException {
+    public static String savePhoto(PostForm form) throws IOException {
 
         if (!Files.exists(uploadDir)) {
             Files.createDirectories(uploadDir);
@@ -25,6 +30,7 @@ public class FileUploadUtil {
 
         String username = form.getUsername();
         MultipartFile image = form.getImage();
+        String extension = FilenameUtils.getExtension(image.getOriginalFilename());
 
         InputStream inputStream = image.getInputStream();
         BufferedImage inputImage = ImageIO.read(inputStream);
@@ -34,10 +40,17 @@ public class FileUploadUtil {
         graphics.drawImage(inputImage, 0, 0, 220, 160, null);
         graphics.dispose();
 
-        Path filePath = uploadDir.resolve(username + ".jpg");
+        Path filePath = uploadDir.resolve(username + "." + extension);
 
-        ImageIO.write(outputImage, "jpg", new File(String.valueOf(filePath)));
+        ImageIO.write(outputImage, extension, new File(String.valueOf(filePath)));
 
+        return extension;
     }
 
+    public static void deletePhoto(String fileName) throws NoSuchFileException {
+        File file = new File("images/" + fileName);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 }
