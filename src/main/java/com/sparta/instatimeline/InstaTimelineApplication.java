58423fd69ef036d32299c31ee375416b54602efd
wooyongjeong <wooyong.dev@gmail.com>
2021-08-20 15:50:51 +0900

    TimeZone 서울로 설정, title 대문자로 변경

--- src/main/java/com/sparta/instatimeline/InstaTimelineApplication.java
+++ src/main/java/com/sparta/instatimeline/InstaTimelineApplication.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class InstaTimelineApplication {
+
+	@PostConstruct
+	public void timeZoneSetting() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstaTimelineApplication.class, args);
